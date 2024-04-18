d01ce9c668bac8b778d5d7f70bd92b90102bad06
LawrenceDai <lawrend@umich.edu>
2024-04-18 00:41:35 -0400

    Update sort.js

--- js/sort.js
+++ js/sort.js
@@ -13,6 +13,7 @@ var sort_order;
 // var for iterating for loop
 var i;
 
+// sort functions
 function sort_by_name (mylist) {
     mylist.sort(function (a, b){
         return a.dataset.name.localeCompare(b.dataset.name);
@@ -36,12 +37,14 @@ const name_button =  document.querySelector("#name");
 const release_button = document.querySelector("#release");
 const sold_button = document.querySelector("#sold");
 
+// reorder the flex
 function reorder() {
     for (i = 0; i < console_list.length; i++) {
         console_list[i].style.order = i;
     }
 }
 
+// button functions
 name_button.addEventListener("click", function () {
     if (!name_sort) {
         sort_by_name(console_list);
@@ -49,13 +52,13 @@ name_button.addEventListener("click", function () {
         release_sort = false;
         sold_sort = false;
         sort_order = true;
+        release_button.innerHTML = "Release Date";
+        sold_button.innerHTML = "Units Sold";
     } else {
         console_list = console_list.reverse();
         sort_order = !sort_order;
     }
     name_button.innerHTML = "Name" + (sort_order ? ' &#x2193' : ' &#x2191');
-    release_button.innerHTML = "Release Date";
-    sold_button.innerHTML = "Units Sold";
     reorder();
 })
 
@@ -66,13 +69,13 @@ release_button.addEventListener("click", function () {
         release_sort = true;
         sold_sort = false;
         sort_order = true;
+        name_button.innerHTML = "Name";
+        sold_button.innerHTML = "Units Sold";
     } else {
         console_list = console_list.reverse();
         sort_order = !sort_order;
     }
-    name_button.innerHTML = "Name";
     release_button.innerHTML = "Release Date" + (sort_order ? ' &#x2193' : ' &#x2191');
-    sold_button.innerHTML = "Units Sold";
     reorder();
 })
 
@@ -83,12 +86,12 @@ sold_button.addEventListener("click", function () {
         release_sort = false;
         sold_sort = true;
         sort_order = true;
+        name_button.innerHTML = "Name";
+        release_button.innerHTML = "Release Date";
     } else {
         console_list = console_list.reverse();
         sort_order = !sort_order;
     }
-    name_button.innerHTML = "Name";
-    release_button.innerHTML = "Release Date";
     sold_button.innerHTML = "Units Sold" + (sort_order ? ' &#x2193' : ' &#x2191');
     reorder();
 })
